a25bd71adcb5dc3fb00860223ded5566685fac8e
ankammarao <aa.aar225@gmail.com>
2023-11-03 13:44:41 +0000

    Authentication Functionality

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <>
+    <ul className="header-list-container">
+      <Link to="/">
+        <li className="list-item">Home</li>
+      </Link>
+      <Link to="/about">
+        <li className="list-item">About</li>
+      </Link>
+    </ul>
+  </>
+)
+
+export default Header
